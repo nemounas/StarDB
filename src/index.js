9fcf961a987da1df8b9c738de4b838775d67c3da
nemounas <skochew@yandex.ru>
2019-09-30 12:27:43 +0300

    Class SwapiApi

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class SwapiApi {
 
+    _urlBase = "https://swapi.co/api";
+   
+    async GetResorses (url) {
+       
+        const res = await fetch(`${this._urlBase}${url}`);
+       
+        if(!res.ok){
+            throw new Error(`cold not fetch. res.status: ${res.status}`)
+        }
+        const body = await res.json();
+        return body;
+    }
+    async getAllPeople(){
+        const res = await this.GetResorses(`/people/`)
+        return res.results;
+    }
+    getPerson(id){
+        
+        return this.GetResorses(`/people/${id}`);
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const swapico = new SwapiApi()
+swapico.getPerson(5).then((people) => {
+    console.log(people.name)
+})
+
+ 
+ReactDOM.render(<div>Hello</div>, document.getElementById('root'));
 
 
